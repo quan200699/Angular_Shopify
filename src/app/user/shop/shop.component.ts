137cb21a16e12e44bc7e5e88690be11a454d6f6f
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-25 10:58:07 +0700

    shop layout

--- src/app/user/shop/shop.component.ts
+++ src/app/user/shop/shop.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Category} from "../../model/category";
+import {UserToken} from "../../model/user-token";
+import {CategoryService} from "../../service/category/category.service";
+import {AuthenticationService} from "../../service/auth/authentication.service";
+import {Router} from "@angular/router";
+import {Product} from "../../model/product";
+import {ProductService} from "../../service/product/product.service";
 
 @Component({
   selector: 'app-shop',
@@ -6,10 +13,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
+  listCategory: Category[] = [];
+  currentUser: UserToken;
+  hasRoleAdmin = false;
+  listProduct: Product[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private categoryService: CategoryService,
+              private authenticationService: AuthenticationService,
+              private productService: ProductService,
+              private router: Router) {
+    this.authenticationService.currentUser.subscribe(value => this.currentUser = value);
+    if (this.currentUser) {
+      const roleList = this.currentUser.roles;
+      for (const role of roleList) {
+        if (role.authority === 'ROLE_ADMIN') {
+          this.hasRoleAdmin = true;
+        }
+      }
+    }
   }
 
+  ngOnInit() {
+    this.getAllCategories();
+    this.getAllProduct();
+  }
+
+  getAllProduct() {
+    this.productService.getAllProduct().subscribe(listProduct => {
+      this.listProduct = listProduct;
+      this.listProduct.map(async product => {
+        product.image = await this.getAllImageByProduct(product);
+      })
+    })
+  }
+
+  getAllImageByProduct(product: Product) {
+    return this.productService.getAllImageByProduct(product.id).toPromise()
+  }
+
+  getAllCategories() {
+    this.categoryService.getAllCategory().subscribe(listCategory => {
+      this.listCategory = listCategory;
+    })
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
