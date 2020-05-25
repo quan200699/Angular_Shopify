149bac29c458d1f8725e9435262c69e570648496
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-25 17:35:34 +0700

    fix layout

--- src/app/user/homepage/homepage.component.ts
+++ src/app/user/homepage/homepage.component.ts
@@ -37,8 +37,50 @@ export class HomepageComponent implements OnInit {
 
   ngOnInit() {
     $(document).ready(function(){
-      $('.owl-carousel').owlCarousel();
+      $(".latest-product__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+      });
+      $(".categories__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 4,
+        dots: false,
+        nav: true,
+        navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+
+          0: {
+            items: 1,
+          },
+
+          480: {
+            items: 2,
+          },
+
+          768: {
+            items: 3,
+          },
+
+          992: {
+            items: 4,
+          }
+        }
+      });
     });
+
     this.getAllProduct();
     this.getAllCategories();
   }
