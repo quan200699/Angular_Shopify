15547c250df4d31b05a8ea78d619a525758429c5
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-26 08:30:37 +0700

    product detail component added

--- src/app/user/product-detail/product-detail.component.ts
+++ src/app/user/product-detail/product-detail.component.ts
@@ -2,6 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {Category} from "../../model/category";
 import {FormControl, FormGroup} from "@angular/forms";
 import {CategoryService} from "../../service/category/category.service";
+import {Product} from "../../model/product";
+import {ProductService} from "../../service/product/product.service";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+
+declare var $: any;
 
 @Component({
   selector: 'app-product-detail',
@@ -13,11 +19,58 @@ export class ProductDetailComponent implements OnInit {
   searchForm: FormGroup = new FormGroup({
     name: new FormControl('')
   })
+  currentProduct: Product;
+  sub: Subscription
 
-  constructor(private categoryService: CategoryService) {
+  constructor(private categoryService: CategoryService,
+              private productService: ProductService,
+              private activatedRoute: ActivatedRoute) {
+    this.sub = this.activatedRoute.paramMap.subscribe(async (paramMap: ParamMap) => {
+      const id = +paramMap.get('id');
+      this.currentProduct = await this.getProduct(id);
+      this.currentProduct.image = await this.getAllImageByProduct(this.currentProduct);
+    })
   }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $(".product__details__pic__slider").owlCarousel({
+        loop: true,
+        margin: 20,
+        items: 4,
+        dots: true,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+      });
+      $('.product__details__pic__slider img').on('click', function () {
+        var imgurl = $(this).data('imgbigurl');
+        var bigImg = $('.product__details__pic__item--large').attr('src');
+        if (imgurl != bigImg) {
+          $('.product__details__pic__item--large').attr({
+            src: imgurl
+          });
+        }
+      });
+      var proQty = $('.pro-qty');
+      proQty.prepend('<span class="dec qtybtn">-</span>');
+      proQty.append('<span class="inc qtybtn">+</span>');
+      proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+          var newVal = parseFloat(oldValue) + 1;
+        } else {
+          // Don't allow decrementing below zero
+          if (oldValue > 0) {
+            var newVal = parseFloat(oldValue) - 1;
+          } else {
+            newVal = 0;
+          }
+        }
+        $button.parent().find('input').val(newVal);
+      });
+    })
     this.getAllCategories();
   }
 
@@ -25,5 +78,13 @@ export class ProductDetailComponent implements OnInit {
     this.categoryService.getAllCategory().subscribe(listCategory => {
       this.listCategory = listCategory;
     })
+  }
+
+  getProduct(id: number) {
+    return this.productService.getProduct(id).toPromise();
+  }
+
+  getAllImageByProduct(product: Product) {
+    return this.productService.getAllImageByProduct(product.id).toPromise()
   }
 }
