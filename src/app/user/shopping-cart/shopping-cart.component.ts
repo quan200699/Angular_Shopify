4070c09cc5ed0ab241115e47fd89512e3ae865d8
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-26 12:48:07 +0700

    shopping cart jquery

--- src/app/user/shopping-cart/shopping-cart.component.ts
+++ src/app/user/shopping-cart/shopping-cart.component.ts
@@ -32,6 +32,27 @@ export class ShoppingCartComponent implements OnInit {
   }
 
   ngOnInit() {
+    $(document).ready(function () {
+      var proQty = $('.pro-qty');
+      proQty.prepend('<span class="dec qtybtn">-</span>');
+      proQty.append('<span class="inc qtybtn">+</span>');
+      proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+          var newVal = parseFloat(oldValue) + 1;
+        } else {
+          // Don't allow decrementing below zero
+          if (oldValue > 0) {
+            var newVal = parseFloat(oldValue) - 1;
+          } else {
+            newVal = 0;
+          }
+        }
+        $button.parent().find('input').val(newVal);
+      });
+
+    })
     this.getAllCategories();
     this.activatedRoute.params.subscribe(async params => {
       var id = params['id'];
