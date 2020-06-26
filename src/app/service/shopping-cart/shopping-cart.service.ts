7711dc001e3b430d21837c04e73fe673fbb14581
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-06-26 13:49:04 +0700

    add item to cart using db

--- src/app/service/shopping-cart/shopping-cart.service.ts
+++ src/app/service/shopping-cart/shopping-cart.service.ts
@@ -3,6 +3,7 @@ import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ShoppingCart} from "../../model/shopping-cart";
+import {Item} from "../../model/item";
 
 const API_URL = `${environment.apiUrl}`
 
@@ -26,11 +27,19 @@ export class ShoppingCartService {
     return this.http.get<ShoppingCart>(API_URL + `/carts/${id}`)
   }
 
+  getCartByUser(id: number): Observable<ShoppingCart> {
+    return this.http.get<ShoppingCart>(API_URL + `/carts/users/${id}`)
+  }
+
   updateCart(id: number, shoppingCart: ShoppingCart): Observable<ShoppingCart> {
     return this.http.put<ShoppingCart>(API_URL + `/carts/${id}`, shoppingCart)
   }
 
   deleteShoppingCart(id: number): Observable<ShoppingCart> {
     return this.http.delete<ShoppingCart>(API_URL + `/carts/${id}`);
+  }
+
+  getAllItemByShoppingCart(id: number): Observable<Item[]> {
+    return this.http.get<Item[]>(API_URL + `/carts/${id}/items`);
   }
 }
--- src/app/user/homepage/homepage.component.ts
+++ src/app/user/homepage/homepage.component.ts
@@ -5,8 +5,15 @@ import {CategoryService} from "../../service/category/category.service";
 import {Category} from "../../model/category";
 import {FormControl, FormGroup} from "@angular/forms";
 import {Router} from "@angular/router";
+import {AuthenticationService} from "../../service/auth/authentication.service";
+import {UserToken} from "../../model/user-token";
+import {ShoppingCartService} from "../../service/shopping-cart/shopping-cart.service";
+import {ShoppingCart} from "../../model/shopping-cart";
+import {Item} from "../../model/item";
+import {ItemService} from "../../service/item/item.service";
 
 declare var $: any;
+declare var Swal: any;
 
 @Component({
   selector: 'app-homepage',
@@ -20,10 +27,19 @@ export class HomepageComponent implements OnInit {
     name: new FormControl('')
   })
   listProductLatest: Product[] = [];
+  currentUser: UserToken;
+  shoppingCart: ShoppingCart;
 
   constructor(private productService: ProductService,
               private categoryService: CategoryService,
+              private authenticationService: AuthenticationService,
+              private shoppingCartService: ShoppingCartService,
+              private itemService: ItemService,
               private router: Router) {
+    this.authenticationService.currentUser.subscribe(value => {
+      this.currentUser = value
+      this.getShoppingCartByUser(this.currentUser.id);
+    });
   }
 
   ngOnInit() {
@@ -77,6 +93,92 @@ export class HomepageComponent implements OnInit {
     this.getAllProductLatest();
     this.getAllProduct();
     this.getAllCategories();
+  }
+
+  getShoppingCartByUser(id: number) {
+    this.shoppingCartService.getCartByUser(id).subscribe(shoppingCart => {
+      this.shoppingCart = shoppingCart;
+    })
+  }
+
+  getAllItemInShoppingCart(id: number) {
+    return this.shoppingCartService.getAllItemByShoppingCart(id).toPromise();
+  }
+
+  async addItemToShoppingCart(productId: number, shoppingCartId: number) {
+    let items = await this.getAllItemInShoppingCart(shoppingCartId);
+    if (items.length == 0) {
+      let item: Item = {
+        product: {
+          id: productId
+        },
+        quantity: 1,
+        shoppingCart: {
+          id: shoppingCartId
+        }
+      }
+      this.itemService.createItem(item).subscribe(() => {
+        $(function () {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000
+          });
+
+          Toast.fire({
+            type: 'success',
+            title: 'Đã thêm vào giỏ hàng'
+          });
+        });
+      });
+    }else {
+      items.map(item => {
+        if (item.product.id == productId) {
+          item.quantity += 1;
+          this.itemService.updateItem(item.id, item).subscribe(() => {
+            $(function () {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+              });
+
+              Toast.fire({
+                type: 'success',
+                title: 'Đã thêm vào giỏ hàng'
+              });
+            });
+          });
+        } else {
+          let item1: Item = {
+            product: {
+              id: productId
+            },
+            quantity: 1,
+            shoppingCart: {
+              id: shoppingCartId
+            }
+          }
+          this.itemService.createItem(item1).subscribe(() => {
+            $(function () {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+              });
+
+              Toast.fire({
+                type: 'success',
+                title: 'Đã thêm vào giỏ hàng'
+              });
+            });
+          });
+        }
+      })
+    }
   }
 
   getAllProduct() {
