fa2326d64a168a64a9fc567a7efcf77ee667620c
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-06-27 09:43:10 +0700

    add item to shopping cart

--- src/app/user/shopping-cart/shopping-cart.component.ts
+++ src/app/user/shopping-cart/shopping-cart.component.ts
@@ -8,6 +8,11 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {Product} from "../../model/product";
 import {WarehouseBillDetailService} from "../../service/warehouse-bill-detail/warehouse-bill-detail.service";
 import {OrdersService} from "../../service/order/orders.service";
+import {AuthenticationService} from "../../service/auth/authentication.service";
+import {ShoppingCartService} from "../../service/shopping-cart/shopping-cart.service";
+import {ShoppingCart} from "../../model/shopping-cart";
+import {UserToken} from "../../model/user-token";
+import {ItemService} from "../../service/item/item.service";
 
 declare var $: any;
 declare var Swal: any;
@@ -27,13 +32,65 @@ export class ShoppingCartComponent implements OnInit {
   })
   items: Item[] = [];
   total: number = 0;
+  shoppingCart: ShoppingCart;
+  currentUser: UserToken;
 
   constructor(private categoryService: CategoryService,
               private activatedRoute: ActivatedRoute,
               private productService: ProductService,
               private warehouseBillDetailService: WarehouseBillDetailService,
               private ordersService: OrdersService,
+              private authenticationService: AuthenticationService,
+              private shoppingCartService: ShoppingCartService,
+              private itemService: ItemService,
               private router: Router) {
+
+    this.getAllCategories();
+    this.loadCart();
+    this.authenticationService.currentUser.subscribe(value => {
+      this.currentUser = value
+      this.getShoppingCartByUser(this.currentUser.id);
+    });
+    if (this.currentUser == null) {
+      this.activatedRoute.params.subscribe(async params => {
+        var id = params['id'];
+        if (id) {
+          const product = await this.getProduct(id);
+          product.image = await this.getAllImageByProduct(product)
+          var item: Item = {
+            product: product,
+            quantity: 1
+          };
+          const sum = await this.sumAmountInput(product.id);
+          if (sum == null) {
+            $(function () {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+              });
+
+              Toast.fire({
+                type: 'error',
+                title: 'Mặt hàng này không còn vui lòng chọn mua sản phẩm khác'
+              });
+            });
+          } else {
+            if (localStorage.getItem('cart') == null) {
+              let cart: any = [];
+              cart.push(JSON.stringify(item));
+              localStorage.setItem('cart', JSON.stringify(cart));
+            } else {
+              this.addProductToCart(id, item);
+            }
+          }
+          this.loadCart();
+        } else {
+          this.loadCart();
+        }
+      });
+    }
   }
 
   ngOnInit() {
@@ -58,46 +115,29 @@ export class ShoppingCartComponent implements OnInit {
         $button.parent().find('input').val(newVal);
       });
     })
-    this.getAllCategories();
-    this.loadCart();
-    this.activatedRoute.params.subscribe(async params => {
-      var id = params['id'];
-      if (id) {
-        const product = await this.getProduct(id);
-        product.image = await this.getAllImageByProduct(product)
-        var item: Item = {
-          product: product,
-          quantity: 1
-        };
-        const sum = await this.sumAmountInput(product.id);
-        if (sum == null) {
-          $(function () {
-            const Toast = Swal.mixin({
-              toast: true,
-              position: 'top-end',
-              showConfirmButton: false,
-              timer: 3000
-            });
+  }
 
-            Toast.fire({
-              type: 'error',
-              title: 'Mặt hàng này không còn vui lòng chọn mua sản phẩm khác'
-            });
-          });
-        } else {
-          if (localStorage.getItem('cart') == null) {
-            let cart: any = [];
-            cart.push(JSON.stringify(item));
-            localStorage.setItem('cart', JSON.stringify(cart));
-          } else {
-            this.addProductToCart(id, item);
-          }
-        }
-        this.loadCart();
-      } else {
-        this.loadCart();
-      }
-    });
+  getShoppingCartByUser(id: number) {
+    if (id == null) {
+      this.loadCart();
+    } else {
+      this.shoppingCartService.getCartByUser(id).subscribe(shoppingCart => {
+        this.shoppingCart = shoppingCart;
+        this.shoppingCartService.getAllItemByShoppingCart(this.shoppingCart.id).subscribe(items => {
+          this.items = items;
+          this.sumTotalPrice();
+        })
+      })
+    }
+  }
+
+  async addItemToShoppingCart(productId: number, shoppingCartId: number) {
+    let items = await this.getAllItemInShoppingCart(shoppingCartId);
+    this.itemService.addItemToShoppingCart(items, productId, shoppingCartId);
+  }
+
+  getAllItemInShoppingCart(id: number) {
+    return this.shoppingCartService.getAllItemByShoppingCart(id).toPromise();
   }
 
   sumAmountInput(id: number) {
@@ -178,7 +218,6 @@ export class ShoppingCartComponent implements OnInit {
   }
 
   loadCart(): void {
-    this.total = 0;
     this.items = [];
     let cart = JSON.parse(localStorage.getItem('cart'));
     for (var i = 0; i < cart.length; i++) {
@@ -187,8 +226,16 @@ export class ShoppingCartComponent implements OnInit {
         product: item.product,
         quantity: item.quantity
       });
+    }
+    this.sumTotalPrice();
+  }
+
+  sumTotalPrice(): number {
+    this.total = 0;
+    for (let item of this.items) {
       this.total += (item.product.price * (1 - item.product.saleOff / 100)) * item.quantity;
     }
+    return this.total;
   }
 
   remove(id: number): void {
