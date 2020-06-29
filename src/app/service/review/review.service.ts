299935801c260d35ab9e3d21f4ab60890287567a
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-06-29 17:29:29 +0700

    add review service

---
+++ src/app/service/review/review.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Review} from "../../model/review";
+
+const API_URL = `${environment.apiUrl}`
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllReview(): Observable<Review[]> {
+    return this.http.get<Review[]>(API_URL + '/reviews');
+  }
+
+  createReview(review: Review): Observable<Review> {
+    return this.http.post<Review>(API_URL + '/reviews', review);
+  }
+
+  updateReview(id: number, review: Review): Observable<Review> {
+    return this.http.put<Review>(API_URL + `/reviews/${id}`, review)
+  }
+
+  deleteReview(id: number): Observable<Review> {
+    return this.http.delete<Review>(API_URL + `/reviews/${id}`)
+  }
+
+  getReview(id: number): Observable<Review> {
+    return this.http.get<Review>(API_URL + `/reviews/${id}`)
+  }
+}
