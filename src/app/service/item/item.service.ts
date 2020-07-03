4200f85b369b451f856836c068c2e4f5312138cd
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-07-03 13:54:52 +0700

    add contraint when add item to cart

--- src/app/service/item/item.service.ts
+++ src/app/service/item/item.service.ts
@@ -3,6 +3,8 @@ import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Item} from "../../model/item";
+import {WarehouseBillDetailService} from "../warehouse-bill-detail/warehouse-bill-detail.service";
+import {OrderDetailService} from "../order-detail/order-detail.service";
 
 const API_URL = `${environment.apiUrl}`
 
@@ -14,7 +16,9 @@ declare var Swal: any;
 })
 export class ItemService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private warehouseBillDetailService: WarehouseBillDetailService,
+              private orderDetailService: OrderDetailService) {
   }
 
   getAllItem(): Observable<Item[]> {
@@ -37,43 +41,25 @@ export class ItemService {
     return this.http.delete<Item>(API_URL + `/items/${id}`);
   }
 
-  addItemToShoppingCart(items: any, productId: number, shoppingCartId: number) {
-    if (items.length == 0) {
-      let item: Item = {
-        product: {
-          id: productId
-        },
-        quantity: 1,
-        shoppingCart: {
-          id: shoppingCartId
-        }
-      }
-      this.createItem(item).subscribe(() => {
-
-        $(function () {
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000
-          });
-
-          Toast.fire({
-            type: 'success',
-            title: 'Đã thêm vào giỏ hàng'
-          });
+  async addItemToShoppingCart(items: any, productId: number, shoppingCartId: number) {
+    let sumInput = await this.warehouseBillDetailService.sumAmount(productId).toPromise();
+    let sumOutput = await this.orderDetailService.sumAmount(productId).toPromise();
+    if (sumInput - sumOutput <= 0) {
+      $(function () {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000
+        });
+        Toast.fire({
+          type: 'error',
+          title: 'Sản phẩm này hiện đã hết!\nHãy chon mua sản phẩm khác'
         });
       });
     } else {
-      let index = -1;
-      for (let i = 0; i < items.length; i++) {
-        if (items[i].product.id == productId) {
-          index = i;
-          break;
-        }
-      }
-      if (index == -1) {
-        let item1: Item = {
+      if (items.length == 0) {
+        let item: Item = {
           product: {
             id: productId
           },
@@ -82,7 +68,7 @@ export class ItemService {
             id: shoppingCartId
           }
         }
-        this.createItem(item1).subscribe(() => {
+        this.createItem(item).subscribe(() => {
           $(function () {
             const Toast = Swal.mixin({
               toast: true,
@@ -98,21 +84,55 @@ export class ItemService {
           });
         });
       } else {
-        items[index].quantity += 1;
-        this.updateItem(items[index].id, items[index]).subscribe(() => {
-          $(function () {
-            const Toast = Swal.mixin({
-              toast: true,
-              position: 'top-end',
-              showConfirmButton: false,
-              timer: 3000
-            });
-            Toast.fire({
-              type: 'success',
-              title: 'Đã thêm vào giỏ hàng'
+        let index = -1;
+        for (let i = 0; i < items.length; i++) {
+          if (items[i].product.id == productId) {
+            index = i;
+            break;
+          }
+        }
+        if (index == -1) {
+          let item1: Item = {
+            product: {
+              id: productId
+            },
+            quantity: 1,
+            shoppingCart: {
+              id: shoppingCartId
+            }
+          }
+          this.createItem(item1).subscribe(() => {
+            $(function () {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+              });
+
+              Toast.fire({
+                type: 'success',
+                title: 'Đã thêm vào giỏ hàng'
+              });
             });
           });
-        });
+        } else {
+          items[index].quantity += 1;
+          this.updateItem(items[index].id, items[index]).subscribe(() => {
+            $(function () {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+              });
+              Toast.fire({
+                type: 'success',
+                title: 'Đã thêm vào giỏ hàng'
+              });
+            });
+          });
+        }
       }
     }
   }
