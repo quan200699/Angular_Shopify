e92cc221b88f37dc255bff526ee7465df784b02c
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-07-08 16:20:16 +0700

    add notification service

---
+++ src/app/service/notification/notification.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+const API_URL = `${environment.apiUrl}`
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllNotification(): Observable<Notification[]> {
+    return this.http.get<Notification[]>(API_URL + '/notifications');
+  }
+
+  createNotification(notification: Notification): Observable<Notification> {
+    return this.http.post<Notification>(API_URL + '/notifications', notification);
+  }
+
+  updateCustomerInfo(id: number, notification: Notification): Observable<Notification> {
+    return this.http.put<Notification>(API_URL + `/notifications/${id}`, notification)
+  }
+
+  deleteNotification(id: number): Observable<Notification> {
+    return this.http.delete<Notification>(API_URL + `/notifications/${id}`)
+  }
+
+  getNotification(id: number): Observable<Notification> {
+    return this.http.get<Notification>(API_URL + `/notifications/${id}`)
+  }
+}
